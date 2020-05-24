98e1f37323ccae6d367f0fc6d425ff19ded2ec32
Emilio Graff <1@emil.io>
2020-05-24 15:37:40 -0700

    Module to fetch with cache (expires on the hour)

---
+++ fetch-with-cache.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const md5 = require('md5');
+const fs = require('fs');
+
+function makeCacheFilename(url) {
+    const key = `${url}${moment().format('YYYY-MM-DD_HH')}`
+    return `cache/${md5(key)}`;   
+}
+
+async function getHtml(url) {
+    const filename = `${makeCacheFilename(url)}.html`;
+    if (fs.existsSync(filename)) {
+        return fs.readFileSync(filename);
+    }
+
+    const response = await fetch(url);
+    const data = await response.text();
+    fs.writeFile(filename, data, err => {
+        if (err) {
+            console.error(`Could not write cache file ${filename} for ${url}`);
+        }
+    });
+    return data;
+}
+
+async function getJson(url) {
+    const filename = `${makeCacheFilename(url)}.json`;
+    if (fs.existsSync(filename)) {
+        return JSON.parse(fs.readFileSync(filename));
+    }
+
+    const response = await fetch(url);
+    const data = await response.json();
+    fs.writeFile(filename, JSON.stringify(data, null, 2), err => {
+        if (err) {
+            console.error(`Could not write cache file ${filename} for ${url}`);
+        }
+    });
+    return data;
+}
+
+module.exports = { getHtml, getJson }
